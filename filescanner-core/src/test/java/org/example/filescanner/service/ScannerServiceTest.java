3358c92e205738909fd459e947754ac13b9dd29f
Martin Gallauner <martin.gallauner@phow.solutions>
2020-11-23 20:00:12 +0100

    adds simple tests

--- filescanner-core/src/test/java/org/example/filescanner/service/ScannerServiceTest.java
+++ filescanner-core/src/test/java/org/example/filescanner/service/ScannerServiceTest.java
@@ -1,34 +1,43 @@
 package org.example.filescanner.service;
 
 import org.example.filescanner.model.NodeDto;
+import org.example.filescanner.persistence.Node;
 import org.example.filescanner.persistence.NodeRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.AdditionalAnswers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ScannerServiceTest {
 
     @InjectMocks
-    ScannerService scannerService;
+    private ScannerService scannerService;
 
     @Mock
-    NodeRepository nodeRepository;
+    private NodeRepository nodeRepository;
 
     @Test
     void scanFolder() {
-        //arrange
+        when(nodeRepository.save(any(Node.class))).then(AdditionalAnswers.returnsFirstArg());
+
         scannerService.setPath(new File("src/test/resources/data").getAbsolutePath());
-
-        //act
         List<NodeDto> result = scannerService.scanFolder();
-
-
-        //assert
+        assertEquals(4, result.size());
+        assertEquals("DIR", result.get(0).getType());
+        assertEquals("data", result.get(0).getName());
+        assertEquals("foto", result.get(1).getName());
+        assertEquals("test2.txt", result.get(2).getName());
+        assertEquals("test1.txt", result.get(3).getName());
     }
 }
